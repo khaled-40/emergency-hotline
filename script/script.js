79c13ff1498d8c5694d448110af87af05a0837be
khaled-40 <khaledmasud.dev@gmail.com>
2025-08-29 15:31:32 +0600

    finished with html and css file

---
+++ script/script.js
@@ -0,0 +1,7 @@
+{/* <div class="flex justify-between items-center p-2 bg-gray-50 rounded-xl mb-2">
+                    <div>
+                        <h3 class="font-bold text-xs">Fire Service Number</h3>
+                        <p class="text-sm mt-2 text-gray-500">999</p>
+                    </div>
+                    <p class="text-sm  text-gray-600">11:36:58 AM</p>
+                </div> */}
